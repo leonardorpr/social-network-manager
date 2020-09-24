25f800e220a86dcb822ae58f6377e75788b02943
Leonardo Pinheiro <leoprocha100@gmail.com>
2020-09-24 20:54:53 -0300

    chore(app): get all assets and set current font-family

---
+++ src/assets/images/index.ts
@@ -0,0 +1,6 @@
+import avatar from './avatar.png';
+import logo from './logo.png';
+import postPreview from './post-preview.png';
+import posts from './posts.png';
+
+export { avatar, logo, postPreview, posts };
