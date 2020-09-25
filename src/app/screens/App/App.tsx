0665fe9a1681fee91d0afb9a163ef9da32854575
Leonardo Pinheiro <leoprocha100@gmail.com>
2020-09-25 18:20:19 -0300

    feat(app): configure theme file

--- src/app/screens/App/App.tsx
+++ src/app/screens/App/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Provider } from 'react-redux';
 
 import { PersistGate } from 'redux-persist/integration/react';
+import { ThemeProvider } from 'styled-components';
+
+import theme from 'configs/theme';
 
 import GlobalStyles from 'assets/styles';
 
@@ -12,8 +15,10 @@ import { MainRoutes } from 'app/routes';
 const App: React.FC = () => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <GlobalStyles />
-      <MainRoutes />
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <MainRoutes />
+      </ThemeProvider>
     </PersistGate>
   </Provider>
 );
---
+++ src/configs/theme/index.ts
@@ -0,0 +1,35 @@
+const theme = {
+  colors: {
+    orange: {
+      100: '#ffe5ca',
+      200: '#f2994a',
+      300: '#ff962b',
+    },
+    blue: {
+      100: '#5a97e8',
+      200: '#2f80ed',
+      300: '#0d6ce8',
+    },
+    accent: {
+      100: '#ffffff',
+      200: '#f8f8f8',
+      300: '#e0e0e0',
+      400: '#dddddd',
+      500: '#9e9e9e',
+      600: '#949494',
+      700: '#828282',
+      800: '#4f4f4f',
+      900: '#333333',
+      1000: '#000000',
+    },
+  },
+  typography: {
+    size: {
+      small: 12,
+      normal: 14,
+      large: 16,
+    },
+  },
+};
+
+export default theme;
